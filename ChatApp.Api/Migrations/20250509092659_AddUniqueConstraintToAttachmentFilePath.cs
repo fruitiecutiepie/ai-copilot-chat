385e4bfd7acbf7b60283e5dc9e61ae851abb14e9
fruitiecutiepie <104437268+fruitiecutiepie@users.noreply.github.com>
2025-05-09 20:50:28 +1000

    add unique contraints for attachment file path

---
+++ ChatApp.Api/Migrations/20250509092659_AddUniqueConstraintToAttachmentFilePath.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ChatApp.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUniqueConstraintToAttachmentFilePath : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_ChatMessageAttachments_FilePath",
+                table: "ChatMessageAttachments",
+                column: "FilePath",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_ChatMessageAttachments_FilePath",
+                table: "ChatMessageAttachments");
+        }
+    }
+}
