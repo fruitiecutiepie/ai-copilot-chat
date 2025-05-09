dd5a38072e91231f8516dcd74ae770b21a0b309d
fruitiecutiepie <104437268+fruitiecutiepie@users.noreply.github.com>
2025-05-09 20:39:41 +1000

    add SignalR and options

--- ChatApp.Api/Program.cs
+++ ChatApp.Api/Program.cs
@@ -1,7 +1,28 @@
+﻿using ChatApp.Api.Hubs;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(opt => opt.AddPolicy("AllowClient", p =>
+{
+  p.WithOrigins("http://localhost:3000")
+   .AllowAnyHeader()
+   .AllowAnyMethod()
+   .AllowCredentials();
+}));
+builder.Services.AddSignalR();
+builder.Services.AddControllers()
+  .AddJsonOptions(opt =>
+  {
+    opt.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+  });
+
 var app = builder.Build();
+
+app.UseWebSockets();
+app.UseCors("AllowClient");
+
+app.MapControllers();
+app.MapHub<ChatHub>("/hubs");
 
 app.Run();
